da69b9e4ac862cb99d596a7d052469f755dcb581
Kruti Parekh <kruti.parekh@openxcell.info>
2017-12-25 12:35:05 +0530

    Package Customization Integration (In Progreess)

---
+++ app/src/main/java/com/cheep/cheepcare/fragment/SelectPackageSpecificationsFragment.java
@@ -0,0 +1,89 @@
+package com.cheep.cheepcare.fragment;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cheep.R;
+import com.cheep.activity.BaseAppCompatActivity;
+import com.cheep.cheepcare.activity.PackageCustomizationActivity;
+import com.cheep.databinding.FragmentSelectPackageSpecificationBinding;
+import com.cheep.fragment.BaseFragment;
+
+/**
+ * Created by pankaj on 12/25/17.
+ */
+
+public class SelectPackageSpecificationsFragment extends BaseFragment {
+
+    private static final String TAG = SelectPackageSpecificationsFragment.class.getSimpleName();
+    private PackageCustomizationActivity mPackageCustomizationActivity;
+    private FragmentSelectPackageSpecificationBinding mBinding;
+    private boolean isVerified = false;
+
+    public static SelectPackageSpecificationsFragment newInstance() {
+        return new SelectPackageSpecificationsFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_select_package_specification, container, false);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initiateUI();
+        setListener();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        Log.d(TAG, "setUserVisibleHint() called with: isVisibleToUser = [" + isVisibleToUser + "]");
+        if (!isVisibleToUser || mPackageCustomizationActivity == null) {
+            return;
+        }
+
+        if (isVerified) {
+            mPackageCustomizationActivity.setTaskState(PackageCustomizationActivity.STEP_ONE_VERIFIED);
+        } else {
+            mPackageCustomizationActivity.setTaskState(PackageCustomizationActivity.STEP_ONE_UNVERIFIED);
+        }
+
+        // Hide the post task button
+//        mPackageCustomizationActivity.showPostTaskButton(false, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        BaseAppCompatActivity activity = (BaseAppCompatActivity) context;
+        if (activity instanceof PackageCustomizationActivity) {
+            mPackageCustomizationActivity = (PackageCustomizationActivity) activity;
+        }
+    }
+
+    @Override
+    public void initiateUI() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+}
