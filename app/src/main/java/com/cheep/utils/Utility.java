aa6ee6e925acae29b5318206b64e47de9267e293
anandrpatole <anand.patole@cheep.care>
2018-07-13 22:08:53 +0530

    --

--- app/src/main/java/com/cheep/utils/Utility.java
+++ app/src/main/java/com/cheep/utils/Utility.java
@@ -172,7 +172,8 @@ public class Utility {
 
     public static final int DEFAULT_PROFILE_SRC = R.drawable.placeholder_cheep_pro_not_assigned;
    // public static final int DEFAULT_CHEEP_LOGO = R.drawable.ic_cheep_circular_icon;
-     public static final int DEFAULT_CHEEP_LOGO = R.drawable.ic_cheep_pro_logo_square_small;
+     public static final int DEFAULT_CHEEP_LOGO = R.drawable.ic_cheep_roun;
+
 
 //    public static final String DEFAULT_PROFILE_URL = "http://msz.uniklinikum-dresden.de/zkn/images/mitarbeiter/dummy120_dagobert83_female_user_icon.png";//"http://lorempixel.com/200/200/people/";
 
